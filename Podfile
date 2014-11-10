40c90d625d9599a1dd7a554d0b92e5a31220ca95
Stas Volskyi <admin@Mac-Admin-2.local>
2014-11-10 14:40:05 +0200

    added pods

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, "7.1"
+
+target "PTSquared" do
+
+pod 'Facebook-iOS-SDK'
+pod 'RestKit'
+pod 'GoogleAnalytics-iOS-SDK'
+
+end
+
+target "PTSquaredTests" do
+
+end
---
+++ Pods/Pods-PTSquared-GoogleAnalytics-iOS-SDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PTSquared_GoogleAnalytics_iOS_SDK : NSObject
+@end
+@implementation PodsDummy_Pods_PTSquared_GoogleAnalytics_iOS_SDK
+@end
